f78e2b7d304fb59d769ac1ca906d1a6d006023f5
an20190113 <an20190113@student.fon.bg.ac.rs>
2024-06-14 11:50:07 +0200

    smanjivanje healthmeter

--- RaketaX/Assets/Scripts/Raketa.cs
+++ RaketaX/Assets/Scripts/Raketa.cs
@@ -8,8 +8,9 @@ public class Raketa : MonoBehaviour
     private Rigidbody2D rb;
     private Vector2 playerDirection;
     public MeterScript healthMeter;
-    public int currentHealth;
-    public int maxHealth = 80;
+    public float currentHealth;
+    public float maxHealth = 80;
+    public float healthDecreaseRate = 10;
     
     // Start is called before the first frame update
     void Start()
@@ -24,11 +25,28 @@ public class Raketa : MonoBehaviour
     {
         float directionY = Input.GetAxisRaw("Vertical");
         playerDirection = new Vector2(0, directionY).normalized;
+         // Smanjite health tokom vremena
+        currentHealth -= healthDecreaseRate * Time.deltaTime;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth); // Ograničite vrednost između 0 i maxHealth
+
+        // Ažurirajte vrednost slajdera
+        //if (healthSlider != null)
+        //{
+           // healthSlider.value = currentHealth;
+       // }
+
+        // Provera da li je health pao na 0
+        if (currentHealth <= 0)
+        {
+            // Ovde možete dodati logiku koja se dešava kada health padne na 0
+            Debug.Log("Health je 0!");
+        }
+         healthMeter.SetHealth(currentHealth);
     }
 
     void FixedUpdate()
     {
         rb.velocity = new Vector2(0, playerDirection.y * playerSpeed);
-        healthMeter.SetHealth(currentHealth); 
+        
     }
 }
